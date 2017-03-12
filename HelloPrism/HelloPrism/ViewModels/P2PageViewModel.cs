8f13a7358747f48921895b2f2c7c25391eb23ca3
Charles Pao <windperson@gmail.com>
2017-03-12 11:03:29 +0800

    Dialog & Action Sheet Demo

--- HelloPrism/HelloPrism/ViewModels/P2PageViewModel.cs
+++ HelloPrism/HelloPrism/ViewModels/P2PageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,10 +25,27 @@ namespace HelloPrism.ViewModels
             set { SetProperty(ref _param, value); }
         }
 
+        public DelegateCommand SelectCommand { get; set; }
+        public DelegateCommand WarningCommand { get; set; }
 
-        public P2PageViewModel()
+        public readonly IPageDialogService _dialogService;
+        public P2PageViewModel(IPageDialogService dialogService)
         {
+            _dialogService = dialogService;
             Title = "This is P2";
+
+            SelectCommand = new DelegateCommand(async() => {
+                string fooResult = await _dialogService.DisplayActionSheetAsync("Information", "Cancel", null, new string[] { "Option1", "Option2", "Option3" });
+                if (string.IsNullOrEmpty(fooResult) == false)
+                {
+                    Title = fooResult;
+                }
+
+            });
+
+            WarningCommand = new DelegateCommand(() => {
+                _dialogService.DisplayAlertAsync("Warning", "This is DisplayAlertAsync()", "OK", "Cancel");
+            });
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)
