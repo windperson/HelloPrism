3629023abf7c94032b67d827811b0d5424f590a3
Charles Pao <windperson@gmail.com>
2017-03-18 14:03:12 +0800

    Scroll View Demo

--- HelloPrism/HelloPrism/ViewModels/MainPageViewModel.cs
+++ HelloPrism/HelloPrism/ViewModels/MainPageViewModel.cs
@@ -9,9 +9,6 @@ namespace HelloPrism.ViewModels
 {
     public class MainPageViewModel : BindableBase, INavigationAware
     {
-        public DelegateCommand GotoP2Command { get; set; }
-        private readonly INavigationService _navigationService;
-
         private string _title;
         public string Title
         {
@@ -19,23 +16,24 @@ namespace HelloPrism.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        private string _userInput;
-
-        public string UserInput
+        private DateTime _date;
+        public DateTime TheDate
         {
-            get { return _userInput; }
-            set { SetProperty(ref _userInput, value); }
+            get { return _date; }
+            set { SetProperty(ref _date, value); }
         }
 
-
-        public MainPageViewModel(INavigationService navigationService)
+        private TimeSpan _time;
+        public TimeSpan TheTime
         {
-            _navigationService = navigationService;
-            GotoP2Command = new DelegateCommand(async () =>
-            {
-                Title = "I've press the putton";
-                await _navigationService.NavigateAsync($"P2Page?UserInput={UserInput}");
-            });
+            get { return _time; }
+            set { SetProperty(ref _time, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            TheDate = DateTime.Now;
+            TheTime = TheDate.TimeOfDay;
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)
@@ -46,7 +44,9 @@ namespace HelloPrism.ViewModels
         public void OnNavigatedTo(NavigationParameters parameters)
         {
             if (parameters.ContainsKey("title"))
+            {
                 Title = (string)parameters["title"] + " and Prism";
+            }
         }
     }
 }
