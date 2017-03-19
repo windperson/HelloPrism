a9c85e3f68878b62cca12f59afe46454056cc791
Charles Pao <windperson@gmail.com>
2017-03-19 10:02:00 +0800

    modify layout & using INotifyPropertyChanged in entities.

--- HelloPrism/HelloPrism/DataEntities/Student.cs
+++ HelloPrism/HelloPrism/DataEntities/Student.cs
@@ -1,15 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloPrism.DataEntities
 {
-    public class Student
+    public class Student : INotifyPropertyChanged
     {
-        public string ID { get; set; }
-        public string Name { get; set; }
-        public uint Age { get; set; }
+
+        private string _id;
+
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; RaisePropertyChanged(); }
+        }
+
+        private string _name;
+            
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; RaisePropertyChanged(); }
+        }
+
+        private uint _age;
+
+        public uint Age
+        {
+            get { return _age; }
+            set { _age = value; RaisePropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
     }
 }
